7b5c581e0e0ab603e37a43cf13f83b8165a0675b
Ai Kurashina <okamoto.ai1@gmail.com>
2021-10-12 11:44:35 -0400

    first

--- project/project/settings.py
+++ project/project/settings.py
@@ -133,6 +133,8 @@ STATICFILES_DIRS = [
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
